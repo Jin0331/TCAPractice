66e58939b6b2fc563d5c7cb1d59d352a1215aed2
Jin0331 <sempre813@naver.com>
2024-05-29 22:30:21 +0900

    Initial Commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by JinwooLee on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
